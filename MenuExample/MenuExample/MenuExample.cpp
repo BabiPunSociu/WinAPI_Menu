c289654c48b4349eb26b77f674a552730beac8b9
BabiPunSociu <Nguyendung281002@gmail.com>
2023-04-28 13:04:24 +0700

    Dũng

---
+++ MenuExample/MenuExample/MenuExample.cpp
@@ -0,0 +1,165 @@
+
+#include "framework.h"
+#include "MenuExample.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HMENU menuMain,menu_V;
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MENUEXAMPLE, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MENUEXAMPLE));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(129));
+    wcex.hCursor        = LoadCursor(hInstance,MAKEINTRESOURCE(IDC_MAIN));
+    wcex.hbrBackground  = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   menuMain = LoadMenu(hInst, MAKEINTRESOURCE(IDR_MAIN));
+   menu_V = LoadMenu(hInst, MAKEINTRESOURCE(IDR_V));
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,menuMain, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HMENU menuLanguages;
+    static POINT point;
+    switch (message)
+    {
+    case WM_RBUTTONDOWN:
+        menuLanguages = GetSubMenu(menuMain, 1);
+        point.x = LOWORD(lParam);
+        point.y = HIWORD(lParam);
+        ClientToScreen(hWnd, &point);
+        TrackPopupMenu(menuLanguages, TPM_RIGHTBUTTON,
+            point.x, point.y,0, hWnd, NULL);
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case ID_E:
+        case ID_LANGUAGES_ENGLISH:
+            SetMenu(hWnd, menuMain);
+            break;
+        case ID_V:
+        case ID_LANGUAGES_VIETNAMESE:
+            SetMenu(hWnd, menu_V);
+            break;
+        case ID_FILE_CLOSE:
+            SendMessage(hWnd, WM_CLOSE, 0, 0);
+            break;
+        default:
+            break;
+        }
+        break;
+    case WM_CLOSE:
+        if (MessageBox(NULL, TEXT("Do you want to Exit?"), TEXT("Yes or no"), MB_YESNO | MB_ICONQUESTION) == IDYES)
+            PostQuitMessage(0);
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
